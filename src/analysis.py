fixed lgp, added averages

src/analysis.py
@@ -124,21 +124,27 @@ def get_avg_gens(f):
 		#print(p.shape)
 		avgs.append(np.average(p, axis = 0))
 		std_devs.append(np.std(p, axis = 0))
-	return np.array(avgs), np.array(std_devs)
+	avgs = np.array(avgs)
+	std_devs = np.array(std_devs)
+	return avgs, avgs-std_devs, avgs+std_devs
 
 def get_err_ribbon(avgs, stds):
 	return avgs + stds, avgs - stds
-cgp_avgs, cgp_std_devs = get_avg_gens(cgp_base_fit_tracks)
-lgp_avgs, lgp_std_devs = get_avg_gens(lgp_base_fit_tracks)
+cgp_avgs, cgp_s_p, cgp_s_m = get_avg_gens(cgp_base_fit_tracks)
+lgp_avgs, lgp_s_p, lgp_s_m = get_avg_gens(lgp_base_fit_tracks)
 print(cgp_avgs.shape)
 print(lgp_avgs.shape)
+print(lgp_s_p.shape)
 fig, axs = plt.subplots(1, len(f_name), figsize = (20, 5))
 for n in range(len(f_name)):
-	axs[n].plot(cgp_avgs[n])
-	axs[n].plot(lgp_avgs[n])
-	axs[n].fill_between(range(cgp_avgs[n].shape[0]), get_err_ribbon(cgp_avgs[n], cgp_std_devs[n]), )
+	axs[n].plot(cgp_avgs[n], label='CGP(1+4)')
+	axs[n].plot(lgp_avgs[n], label='LGP-RC')
+	axs[n].fill_between(range(cgp_avgs[n].shape[0]), cgp_s_m[n], cgp_s_p[n], alpha = 0.25)
+	axs[n].fill_between(range(lgp_avgs[n].shape[0]), lgp_s_m[n], lgp_s_p[n], alpha = 0.25)
+	axs[n].set_ylim(0, 2)
 	axs[n].set_title(f'{f_name[n]}')
 axs[0].set_ylabel("RMSE")
+axs[0].legend()
 fig.suptitle("Fitness over generations")
 fig.tight_layout()
 plt.show()

src/cgp.py
@@ -200,7 +200,7 @@ output_nodes = random.randint(0, max_n+(inputs+bias), (outputs,), np.int32)
 
 #test = run_output(ind_base, output_nodes, np.array([10.0]))
 p_fit = fitness(train_x_bias, train_y, ind_base, output_nodes)
-fit_track.append(p_fit)
+#fit_track.append(p_fit)
 #print(f"Pre-Run Parent Fitness: {p_fit}")
 for g in range(1, max_g+1):
 	children = [mutate(ind_base.copy(), output_nodes.copy()) for x in range(0, max_c)]

src/lgp.py
@@ -219,6 +219,13 @@ def select(pop, fitnesses, max_p = max_p):
 	if n_tour <=1:
 		n_tour = 2
 	new_parents = []
+	#new_fitnesses = []
+	#print(fitnesses)
+	#print(np.argmin(fitnesses)
+	best_fit_id = np.argmin(fitnesses)
+	best_fit = np.argmin(fitnesses)
+	#print(pop[best_fit_id])
+	new_parents.append(pop[best_fit_id])
 	while len(new_parents) < max_p:
 		contestant_indices = random.choice(range(len(pop)), n_tour, replace = False)
 		#print(pop)
@@ -229,7 +236,7 @@ def select(pop, fitnesses, max_p = max_p):
 		candidate = fight(contestants, c_fitnesses)
 		new_parents.append(candidate)
 	
-	return new_parents
+	return new_parents, best_fit
 
 from numpy import unique
 def clean(pop): # remove consecutive duplicate rules
@@ -249,7 +256,7 @@ fit_track = []
 for i in range(0, max_p):
 	parents.append(generate_ind())
 fitnesses = np.zeros((max_p+max_c),)
-fit_track.append(np.argmin(fitnesses))
+#fit_track.append(np.argmin(fitnesses))
 #sort parents before xover and mutation?
 #select before or after xover?
 for g in range(1, max_g+1):
@@ -264,13 +271,14 @@ for g in range(1, max_g+1):
 	pop = parents+children
 	if any(np.isnan(fitnesses)): #screen out nan values
 		nans = np.isnan(fitnesses)
-		fitnesses[nans] = np.PINF
-	best_i = np.argmin(fitnesses)
-	best_pop = pop[i]
-	best_fit = fitnesses[i]
+		fitnesses[nans] = np.PINF	
+	parents, best_i = select(pop, fitnesses)
+	pop = parents+children
+	#best_i = np.argmin(fitnesses)
+	best_pop = pop[best_i]
+	best_fit = fitnesses[best_i]
 	fit_track.append(best_fit)
-	
-	parents = select(pop, fitnesses)
+
 	
 #fig, ax = plt.subplots()
 #ax = plt.plot(fit_track)